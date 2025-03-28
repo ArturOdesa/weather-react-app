990c958e19609037e4a016d04205c56e00911780
Artur Stoianov <stoianovart@gmail.com>
2025-03-28 11:29:59 +0200

    install sass module, complete .weather section, add bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
+import styles from './App.module.scss'
+
+const key = 'a297fd66dca3f11fad5615916c2dd17c';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [city, setCity] = useState({});
+
+  const cityName = 'Odesa';
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${key}`)
+      .then((response) => response.json())
+      .then(data => setCity(data));
+  }, []);
+
+
+
+  const date = new Date().toLocaleDateString('en-En', { day: 'numeric', month: 'long', year: 'numeric' });
+  console.log(city);
 
   return (
     <>
-      <h1>Hello</h1>
+      {/* <p>{ city?.name}</p> */}
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <section className={styles.weather}>
+              <header className={styles.weather__header}>
+                <h1 className={styles.title}>{cityName}</h1>
+                  <img src={`http://openweathermap.org/img/wn/${city.weather[0]?.icon}@2x.png`} alt="Weather icon" />
+              </header>
+              <article className={styles.weather__body}>
+                <h2>{city.main?.temp.toFixed(0)} °C</h2>
+                <p>{city.weather[0]?.description}</p>
+                <p>{date}</p>
+              </article>  
+            </section>
+            <aside>
+              <h2>Weather details</h2>
+              <ul>
+                <li>Feels like: {city.main?.feels_like.toFixed(0)} °C</li>
+                <li>Humidity: {city.main?.humidity}%
+                </li>
+                <li>Wind: {city.wind?.speed} m/s</li>
+                <li>
+                  Pressure: {city.main?.pressure}
+                </li>
+              </ul>
+            </aside>
+          </main>
+        </div>
+      </div>
     </>
   )
 }
